19ffc55ccddbc9515ef4b95aeebdfe2b271d3251
Zbigniew013 <zbigniew.szalkowski@gmail.com>
2022-12-12 12:44:04 +0100

    show quotes dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QUOTES } from './models/data-base';
+import { Quotation } from './models/quotations';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+  quotes: Quotation[] = QUOTES;
 
   onSwitchForm(): void {
     this.showForm = !this.showForm;
   }
 }
+
+// export const QUOTES = [
+//   {
+//     author: 'Jonathan Carroll',
+//     quotation: 'To, że milczę, nie znaczy, że nie mam nic do powiedzenia.',
+//     votes: -4,
+//   },
